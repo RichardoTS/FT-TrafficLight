9802d43417dca973e7635445a0fedf1738acf1a2
RichardoTS <ri.tapiasaez@gmail.com>
2023-03-02 15:43:55 +0000

    minor changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,22 +6,22 @@ const Home = () => {
 
 	return (
 		<div className="TrafficLight">
-			<span></span>
+			
 			<div onClick={() => setSelectedColour("red")}
 				className={
 					"light red " + (selectedColour === "red" ? "blur" : "")
 				}></div>
-			<span></span>
+			
 			<div onClick={() => setSelectedColour("yellow")}
 				className={
 					"light yellow " + (selectedColour === "yellow" ? "blur" : "")
 				}></div>
-			<span></span>
+			
 			<div onClick={() => setSelectedColour("green")}
 				className={
 					"light green " + (selectedColour === "green" ? "blur" : "")
 				}></div>
-			<span></span>
+			
 		</div>
 	);
 };
